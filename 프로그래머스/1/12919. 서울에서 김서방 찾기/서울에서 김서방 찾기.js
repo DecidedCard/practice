09a73081580271d7b9c456976366422aff21c50b
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-01-08 10:57:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+  var answer = '';
+
+  for (let i = 0; i < seoul.length; i++) {
+    if ('Kim' === seoul[i]) {
+      answer = `김서방은 ${i}에 있다`;
+      break;
+    }
+  }
+  return answer;
+}
