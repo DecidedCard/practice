7a39ad88fcb796fdbf7e14e94bd18cbf27e15802
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-06-10 13:14:52 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,12 @@
+function solution(brown, yellow) {
+    let height = 2;
+    let width = brown/2;
+    let result = -1;
+    while(result!==0){
+        height++;
+        width--;
+        let mul = height*width;
+        result = mul - brown - yellow;
+    }
+    return [width,height];
+}
