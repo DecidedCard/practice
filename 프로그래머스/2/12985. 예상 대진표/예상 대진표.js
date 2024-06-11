aa83e0009f8e9dab05c2557f45b71c10d0142270
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-06-11 15:36:50 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,14 +1,11 @@
 function solution(n,a,b)
 {
-    let answer = 1;
+    let answer = 0;
     
     while(a !== b){
         a = Math.round(a / 2);
         b = Math.round(b / 2);
         answer++
-        if(a === b){
-            answer--
-        }
     }
     return answer;
 }
