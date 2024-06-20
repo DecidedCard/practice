870c412770b7f8ef779b39a0108ad4dad2996b19
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-06-20 14:05:42 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2201.03 ms, Memory: 57 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,16 @@
+function solution(elements) {
+    const set = new Set();
+    
+    for(let i = 1; i <= elements.length; i++){
+        
+        for(let j = 0; j <= elements.length; j++){
+            let sum = 0;
+            
+            for(let k = j; k < j + i; k++){
+                sum += elements[k % elements.length];
+            }
+            set.add(sum);
+        }
+    }
+    return set.size;
+}
