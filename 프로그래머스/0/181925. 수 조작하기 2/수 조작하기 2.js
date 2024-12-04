390c731309aeb2fa5bb79a2d2ad8d70cf8c228df
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-12-04 17:28:08 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.67 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,16 +1,15 @@
-const operation = {
-    "-1" : "w",
-    "1" : "s",
-    "-10" : "d",
-    "10" : "a",
+const convert = {
+    "1" : "w",
+    "-1" : "s",
+    "10" : "d",
+    "-10" : "a",
 }
 
 function solution(numLog) {
     let result = '';
     
     numLog.reduce((acc, el) => {
-        const check = acc - el ? acc - el : el - acc;
-        result += operation[`${check}`];
+        result += convert[`${el - acc}`];
         
         return el;
     })
