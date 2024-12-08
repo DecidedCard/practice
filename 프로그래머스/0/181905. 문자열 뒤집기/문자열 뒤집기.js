4ff105de0211a347483621e586ffb77f5face69f
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2024-12-08 19:10:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,8 +1,4 @@
 function solution(my_string, s, e) {
-    if(s === e){
-        return my_string;
-    }
-    
     const str = my_string.substring(s, e + 1);
     const reverseStr = str.split("").reverse().join("");
     
