7a426bb2de13be5647128c7617f8b1247129693c
DecidedCard <152480392+DecidedCard@users.noreply.github.com>
2025-10-03 20:46:18 +0900

    [level 3] Title: 베스트앨범, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,26 @@
+function solution(genres, plays) {
+    let answer = [];
+    let obj = {};
+    let info = {};
+    
+    for(let i = 0; i < genres.length; i++){
+        obj[genres[i]] = (obj[genres[i]] || 0) + plays[i];
+        info[i] = { play: plays[i], genre: genres[i] };
+    }
+    
+    const sortedGenre = Object.keys(obj).sort((a, b) => obj[b] - obj[a]);
+    
+    sortedGenre.forEach((genre) => {
+        const genreSong = Object.values(info)
+        .filter((song) => song.genre === genre)
+        .sort((a, b) => b.play - a.play)
+        .slice(0, 2);
+        
+        genreSong.forEach(v => {
+            const index = Object.values(info).indexOf(v);
+            answer.push(index);
+        })
+    })
+    
+    return answer;
+}
